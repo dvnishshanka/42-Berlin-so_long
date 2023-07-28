0fa8b8f75d5a4aa93e5dd8ea2c1bd2c8312f07d4
dvnishsha <dvnishshanka@gmail.com>
2023-07-29 00:11:52 +0200

    Revised

--- map_solution.c
+++ map_solution.c
@@ -12,7 +12,81 @@
 
 #include "so_long.h"
 
+static bool	mark_access(int	row, int col, char ***game_copy, t_map map_info)
+{
+	if ((row - 1) > 0 && (*game_copy)[row - 1][col] == '0')
+		(*game_copy)[row - 1][col] = 'X';
+	if ((row + 1) < (map_info.no_of_rows - 1) && (*game_copy)[row - 1][col] == '0')
+		(*game_copy)[row + 1][col] = 'X';
+	if ((col - 1) > 0 && (*game_copy)[row][col - 1] == '0')
+		(*game_copy)[row][col - 1] = 'X';
+	if ((col + 1) < (map_info.row_size - 1) && (*game_copy)[row][col + 1] == '0')
+		(*game_copy)[row][col + 1] = 'X';
+	(*game_copy)[row][col] = 'x';
+	return (false);
+}
 
+static bool	is_reachable(int row, int col, char **game_copy, t_map map_info)
+{
+	if ((row - 1) > 0 && (game_copy)[row - 1][col] == 'x')
+		return (true);
+	if ((row + 1) < (map_info.no_of_rows - 1) && (game_copy)[row + 1][col] == 'x')
+		return (true);
+	if ((col - 1) > 0 && (game_copy)[row][col - 1] == 'x')
+		return (true);
+	if ((col + 1) < (map_info.row_size - 1) && (game_copy)[row][col + 1] == 'x')
+		return (true);
+	return (false);
+}
+
+static	void	chk_reachability(char ***game_copy, t_map map_info)
+{
+	int		i;
+	int		j;
+
+	i = 1;
+	while ((map_info.no_of_rows - 1) > i)
+	{
+		j = 1;
+		while (j < (map_info.row_size - 1))
+		{
+			if (((*game_copy)[i][j] == 'C' || (*game_copy)[i][j] == 'E') && (!is_reachable(i, j, *game_copy, map_info)))
+			{
+				free_map(game_copy, map_info.no_of_rows);
+				print_error("Map cannot be solved");
+			}
+			j++;
+		}
+		i++;
+	}
+	
+}
+
+static void	map_access(char ***game_copy, t_map map_info)
+{
+	int		i;
+	int		j;
+	bool	loop_ended;
+
+	loop_ended = false;
+	while (!loop_ended)
+	{
+		loop_ended = true;
+		i = 1;
+		while ((map_info.no_of_rows - 1) > i)
+		{
+			j = 1;
+			while (j < (map_info.row_size - 1))
+			{
+				if ((*game_copy)[i][j] == 'X' || (*game_copy)[i][j] == 'P')
+					loop_ended = mark_access(i, j, game_copy, map_info);
+				j++;
+			}
+			i++;
+		}
+	}
+	print(*game_copy, map_info);
+}
 
 
 void	chk_solution(char **game, t_map map_info)
@@ -35,6 +109,8 @@ void	chk_solution(char **game, t_map map_info)
 		}
 		i ++;
 	}
-	print(game_copy, map_info);
+	map_access(&game_copy, map_info);
+	chk_reachability(&game_copy, map_info);
+	// print(game_copy, map_info);
 	free_map(&game_copy, map_info.no_of_rows);
 }
